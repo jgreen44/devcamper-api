8a3dd325ccd90eead55d27d7e4d184457a485015
Jason Green <jgreen44@asu.edu>
2023-04-21 19:41:18 -0500

    add prettier and eslint

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
 // Load env vars
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 // Body parser
 app.use(express.json());
 
-
-app.listen()
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
